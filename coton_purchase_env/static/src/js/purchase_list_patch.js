f042a3bfa8064f6797298e05dd99173dd160bca7
Nestoki-work <nestoki9708@gmail.com>
2026-01-22 04:06:00 +0100

    [ADD] generate Excell: instalacion del modulo de compras

--- coton_purchase_env/static/src/js/purchase_list_patch.js
+++ coton_purchase_env/static/src/js/purchase_list_patch.js
@@ -3,38 +3,28 @@ import { ListController } from "@web/views/list/list_controller";
 import { patch } from "@web/core/utils/patch";
 import { useService } from "@web/core/utils/hooks";
 
-console.log("✅ EL PARCHE SE HA CARGADO EN MEMORIA");
+console.log("✅ EL PARCHE SE HA CARGADO EN MEMORIA (V2)");
 
 patch(ListController.prototype, {
     setup() {
         super.setup();
-        this.actionService = useService("action");
+        // ESTO DEBE SALIR AL REFRESCAR LA PÁGINA (sin tocar nada)
+        console.log("👻 LIST CONTROLLER INICIADO. Modelo:", this.props.resModel);
     },
 
     get cogItems() {
-        // 1. Obtenemos los ítems originales de la tuerca
         const items = super.cogItems;
+        console.log("👉 CLICK EN TUERCA DETECTADO en modelo:", this.props.resModel);
 
-        // 2. IMPRIMIR EN CONSOLA QUÉ ESTÁ VIENDO ODOO
-        console.log("👉 ABRIENDO TUERCA. MODELO DETECTADO:", this.props.resModel);
-
-        // 3. Verificamos si coincide con 'purchase.order'
         if (this.props.resModel === 'purchase.order') {
-            console.log("🟢 ¡COINCIDENCIA! AGREGANDO BOTÓN AL MENÚ...");
-
             items.push({
                 name: "import_excel_global",
                 description: "📥 Importar Precios (Excel)",
                 action: () => {
-                    console.log("🚀 EJECUTANDO ACCIÓN DE IMPORTAR");
-                    // Asegúrate de que 'coton-etbois' sea el nombre real de tu carpeta técnica
                     this.actionService.doAction("coton_purchase_env.action_purchase_import_wizard_global");
                 },
             });
-        } else {
-            console.log("🔴 EL BOTÓN NO SE AGREGA PORQUE EL MODELO NO ES 'purchase.order'");
         }
-
         return items;
     }
 });
