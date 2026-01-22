88d8f86eb7ff18ed68fee9b39cc1af107ee15dbb
Nestoki-work <nestoki9708@gmail.com>
2026-01-22 04:25:55 +0100

    [ADD] generate Excell: instalacion del modulo de compras

--- coton_purchase_env/static/src/js/purchase_list_patch.js
+++ coton_purchase_env/static/src/js/purchase_list_patch.js
@@ -14,11 +14,6 @@ patch(ListController.prototype, {
     }
 });
 
-/** @odoo-module */
-import { ListController } from "@web/views/list/list_controller";
-import { patch } from "@web/core/utils/patch";
-import { useService } from "@web/core/utils/hooks";
-
 patch(ListController.prototype, {
     setup() {
         super.setup();
